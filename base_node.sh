b61c3553b9bfffc09a5b01813ec6eacafbea0d27
Dmitry Brigadirov <brigadirov@itsph.ru>
2017-11-10 17:52:02 +0300

    Added verbosity and network restrictions

--- base_node.sh
+++ base_node.sh
@@ -4,15 +4,17 @@ GENESIS="PrivateGenesis.json"
 GETH_PATH="/usr/local/"
 BOOT_NODE="enode://5a4b439c086e6259a265d75e01a1a06f941d0dc89d554d11a04598c22da6a4786278460076e688127f8dfb3a95fa782082da1fbc16cedb6b57d6a76683152f92@192.168.4.109:30301"
 NETWORKID="7131208"
+VERBOSITY="4"
+NETWORK="192.168.4.0/24"
 
 startGethNodeWithRPCBackEnd () {
   if [ ! -z "$3" ]
   then
     echo "Starting [Node $1] with RPC backend and unlock account [$3]"
-    $GETH_PATH/bin/geth --identity "Node $1" --networkid $NETWORKID --port $1 --rpc --rpcapi 'web3,eth,personal,net,debug' --rpccorsdomain "*" --datadir $2 --bootnodes $BOOT_NODE --unlock $3 --password $4 --preload "utils.js" console
+    $GETH_PATH/bin/geth --identity "Node $1" --networkid $NETWORKID --port $1 --rpc --rpcapi 'web3,eth,personal,net,debug' --rpccorsdomain "*" --datadir $2 --bootnodes $BOOT_NODE --netrestrict $NETWORK --nat "none" --syncmode "full" --unlock $3 --password $4 --preload "utils.js" --verbosity $VERBOSITY console
   else
     echo "Starting [Node $1] with RPC backend"
-    $GETH_PATH/bin/geth --identity "Node $1" --networkid $NETWORKID --port $1 --rpc --rpcapi 'web3,eth,personal,net,debug' --rpccorsdomain "*" --datadir $2 --bootnodes $BOOT_NODE --preload "utils.js" console
+    $GETH_PATH/bin/geth --identity "Node $1" --networkid $NETWORKID --port $1 --rpc --rpcapi 'web3,eth,personal,net,debug' --rpccorsdomain "*" --datadir $2 --bootnodes $BOOT_NODE --netrestrict $NETWORK --nat "none"--syncmode "full" --preload "utils.js" --verbosity $VERBOSITY console
   fi
 }
 
@@ -20,10 +22,10 @@ startGethNodeWithoutRPCBackEnd () {
   if [ ! -z "$3" ]
   then
     echo "Starting [Node $1] and unlock account [$3]"
-    $GETH_PATH/bin/geth --identity "Node $1" --networkid $NETWORKID --port $1 --datadir $2 --bootnodes $BOOT_NODE --unlock $3 --password $4 --preload "utils.js" console
+    $GETH_PATH/bin/geth --identity "Node $1" --networkid $NETWORKID --port $1 --datadir $2 --bootnodes $BOOT_NODE --netrestrict $NETWORK --syncmode "full" --nat "none" --unlock $3 --password $4 --preload "utils.js" --verbosity $VERBOSITY console
   else
     echo "Starting [Node $1]"
-    $GETH_PATH/bin/geth --identity "Node $1" --networkid $NETWORKID --port $1 --datadir $2 --bootnodes $BOOT_NODE --preload "utils.js" console
+    $GETH_PATH/bin/geth --identity "Node $1" --networkid $NETWORKID --port $1 --datadir $2 --bootnodes $BOOT_NODE --netrestrict $NETWORK --syncmode "full" --nat "none" --preload "utils.js" --verbosity $VERBOSITY console
   fi
 }
 
